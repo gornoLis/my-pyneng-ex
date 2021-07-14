634cc5f6563718593b8617be2e85ad056f855d31
gornolis <svln.tarasova@gmail.com>
2021-07-14 13:40:18 +0300

    task 6_1 6_2

--- exercises/06_control_structures/task_6_2.py
+++ exercises/06_control_structures/task_6_2.py
@@ -12,3 +12,18 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+ip = input("Введите IP-адрес: ")
+oct1 = int(ip.split(".")[0])
+
+if ip == '0.0.0.0':
+   result = 'unassigned'
+elif ip == '255.255.255.255':
+   result = 'local broadcast'
+elif oct1 >=0 and oct1 <= 223:
+   result = 'unicast'
+elif oct1 >= 224 and oct1 <=239:
+   result = 'multicast'
+else:
+   result = 'unused'
+
+print(result)
