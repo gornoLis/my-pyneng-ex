abc1355eae11c9e32f643d2e0892cc5a9a59a0af
gornolis <svln.tarasova@gmail.com>
2021-07-14 14:31:41 +0300

    tasks 6_2*

--- exercises/06_control_structures/task_6_2a.py
+++ exercises/06_control_structures/task_6_2a.py
@@ -17,3 +17,27 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+ip = input("Введите IP-адрес: ")
+try:
+   octs = ip.split(".")
+   if len(octs) != 4:
+      raise ValueError
+   for oct in octs:
+      if int(oct) < 0 or int(oct) > 255:
+         raise ValueError
+
+   oct1 = int(octs[0])
+
+   if ip == '0.0.0.0':
+      result = 'unassigned'
+   elif ip == '255.255.255.255':
+      result = 'local broadcast'
+   elif oct1 >=0 and oct1 <= 223:
+      result = 'unicast'
+   elif oct1 >= 224 and oct1 <=239:
+      result = 'multicast'
+   else:
+      result = 'unused'
+   print(result)
+except ValueError:
+   print("Неправильный IP-адрес")
